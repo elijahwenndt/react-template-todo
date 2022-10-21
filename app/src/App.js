f96f37deb5d20a42a8270d6cbd63e6588187d5c3
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-21 13:24:15 +0000

    styled BB

--- app/src/App.js
+++ app/src/App.js
@@ -3,61 +3,79 @@ import Filtertodo from "./Filtertodo";
 import Statebutton from "./Statebutton";
 import Submission from "./Submission";
 
-
 function App() {
   const [value, setValue] = useState("");
-  const [todo, setToDo] = useState(()=>{
-    return JSON.parse(localStorage.getItem('data')) || []}); 
+  const [todo, setToDo] = useState(() => {
+    return JSON.parse(localStorage.getItem("data")) || [];
+  });
   const [page, setPage] = useState();
 
   useEffect(() => {
-    localStorage.setItem('data', JSON.stringify(todo))
-  },[todo])
+    localStorage.setItem("data", JSON.stringify(todo));
+  }, [todo]);
 
   // console.log(page);
 
   let completeCounter = todo.filter(
     (totalComplete) => totalComplete.complete
   ).length;
-  console.log(completeCounter);
+  // console.log(completeCounter);
 
   function handleClick(boolean) {
     setPage(boolean);
   }
   return (
     <>
-      <Submission
-        value={value}
-        setValue={setValue}
-        todo={todo}
-        setToDo={setToDo}
-        completeCounter={completeCounter}
-      />
-    <div className="container">
-      <div className="row">
-        <div className="col-6"><Filtertodo todo={todo} page={page} bool={page} setToDo={setToDo} /></div>
+      <div className="container text-center text-white">
+        <div className="row">
+          <div className="col-12 bg-black p-4 rounded-3 bg-opacity-75">
+            <Submission
+              value={value}
+              setValue={setValue}
+              todo={todo}
+              setToDo={setToDo}
+              completeCounter={completeCounter}
+            />
+            <div className="col-12 mt-2">
+              <button
+                onClick={() => {
+                  setToDo(
+                    todo.filter(
+                      (deleteComplete) => deleteComplete.complete === false
+                    )
+                  );
+                }}
+              >
+                delete all completed
+              </button>
+
+              <Statebutton
+                text="accomplished"
+                bool={true}
+                handleClick={handleClick}
+              />
+              <Statebutton
+                text="still to do"
+                bool={false}
+                handleClick={handleClick}
+              />
+              <Statebutton
+                text="all tasks"
+                bool={undefined}
+                handleClick={handleClick}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-      {/* <Filtertodo todo={todo} page={page} bool={page} setToDo={setToDo} /> */}
-      
 
-      <button
-        onClick={() => {
-          setToDo(
-            todo.filter((deleteComplete) => deleteComplete.complete === false)
-          );
-        }}
-      >
-        delete all completed
-      </button>
-
-      <Statebutton text="accomplished" bool={true} handleClick={handleClick} />
-      <Statebutton text="still to do" bool={false} handleClick={handleClick} />
-      <Statebutton
-        text="all tasks"
-        bool={undefined}
-        handleClick={handleClick}
-      />
+      <div className="container text-white text-center">
+        <div className="row">
+          <div className="col-12 my-3 bg-black p-4 rounded-3 bg-opacity-75">
+            <Filtertodo todo={todo} page={page} bool={page} setToDo={setToDo} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
